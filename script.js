8c883db015bc84ee1ebb4a0e82eb8d9b9e6302f0
Gici15 <ervir.lalaj@gmail.com>
2026-05-08 17:10:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const sheetURL =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vTlWtHz4SOh9DhSrhlJkPhT1ws47HJJHU6pnLnTFfJOB01B7PAqStxhmCG-35sgEA/pub?gid=952927519&single=true&output=csv";
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vTlWtHz4SOh9DhSrhlJkPhT1ws47HJJHU6pnLnTFfJOB01B7PAqStxhmCG-35sgEA/pub?gid=2093949561&single=true&output=csv";
 
 async function loadData() {
   const response = await fetch(sheetURL);
